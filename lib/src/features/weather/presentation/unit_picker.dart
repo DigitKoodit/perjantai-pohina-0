07412892954ad3332aad05c565f1c3d561cc37d4
Onnimanni Hannonen <onnari11@gmail.com>
2024-04-05 19:41:43 +0300

    feat: creates project with todo comments for tasks

---
+++ lib/src/features/weather/presentation/unit_picker.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:open_weather_example_flutter/src/constants/app_colors.dart';
+import 'package:open_weather_example_flutter/src/features/weather/application/providers.dart';
+import 'package:open_weather_example_flutter/src/features/weather/domain/temperature_unit.dart';
+
+class UnitPicker extends ConsumerWidget {
+  const UnitPicker({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUnit = ref.watch(temperatureUnitProvider);
+    final allUnits = TemperatureUnit.values;
+
+    // TODO(anyone): Implement a DropdownButton to select a temperature unit and update temperatureUnitProvider based on the user input
+    // Hint: You can use DropdownButton widget from Flutter SDK
+    return const Placeholder();
+  }
+}
---
+++ lib/src/features/weather/presentation/weather_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_example_flutter/src/constants/app_colors.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/city_search_box.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/current_weather.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/unit_picker.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/weekly_weather.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key, required this.city});
+  final String city;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.rainGradient,
+          ),
+        ),
+        child: const SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // TODO(anyone): Display Digit logo here
+              // Hint: The asset is already in the project at assets/digit.png
+              Spacer(),
+              UnitPicker(),
+              Spacer(),
+              CitySearchBox(),
+              Spacer(flex: 2),
+              CurrentWeather(),
+              Spacer(flex: 2),
+              WeeklyWeather(),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
